1a541d7e63bc9be398d6cf9755aee2ed50439804
Newave-agc <76753419+Newave-agc@users.noreply.github.com>
2022-11-14 17:23:35 +0200

    deploy fix

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const creds = {username: "user", password: '123password123'};
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const creds = { username: "user", password: "123password123" };
 const uri = `mongodb+srv://${creds.username}:${creds.password}@cluster0.ats7z.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-
-module.exports = uri, client
+(module.exports = uri), client;
